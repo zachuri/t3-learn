cb874ff8b3f62a234c8728d7f59237fcd125164b
zachuri <punsalang.zachary@gmail.com>
2022-09-01 20:44:09 -0700

    added all 4 pages: progress, exercise, weight, diet

---
+++ src/pages/diet.tsx
@@ -0,0 +1,13 @@
+const Exercise = () => {
+	return (
+		<>
+			<main className="container mx-auto flex flex-col items-center justify-center min-h-screen p-4">
+				<h1 className="text-5xl md:text-[5rem] leading-normal font-extrabold text-gray-700">
+					Diet Page
+				</h1>
+			</main>
+		</>
+	);
+};
+
+export default Exercise;
---
+++ src/pages/weight.tsx
@@ -0,0 +1,15 @@
+import Head from "next/head";
+
+const Progress = () => {
+	return (
+		<>
+			<main className="container mx-auto flex flex-col items-center justify-center min-h-screen p-4">
+				<h1 className="text-5xl md:text-[5rem] leading-normal font-extrabold text-gray-700">
+					Weight
+				</h1>
+			</main>
+		</>
+	);
+};
+
+export default Progress;
