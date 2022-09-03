510a89e49318c9963af4c3547aa0c0b45773aff7
zachuri <punsalang.zachary@gmail.com>
2022-09-03 01:47:02 -0700

    weightPage: added UI -> need to dynamically update page

--- src/pages/weight.tsx
+++ src/pages/weight.tsx
@@ -4,8 +4,11 @@ import { useRouter } from "next/router";
 import { trpc } from "../utils/trpc";
 import { CreateWeightInput } from "../schema/weight.schema";
 
+
 const Progress = () => {
 	const { handleSubmit, register, reset } = useForm<CreateWeightInput>();
+
+	const { data, isLoading } = trpc.useQuery(["weights.getAllWeights"]);
 
 	const router = useRouter();
 
@@ -19,27 +22,74 @@ const Progress = () => {
 
 	function onSubmit(values: CreateWeightInput) {
 		mutate(values);
-	}
+  }
 
 	return (
 		<>
 			<main className="container mx-auto flex flex-col items-center justify-center min-h-screen p-4">
-				{/* <h1 className="text-5xl md:text-[5rem] leading-normal font-extrabold text-gray-700">
-					Weight
-				</h1> */}
 				<form onSubmit={handleSubmit(onSubmit)}>
-					{error && error.message}
-					<h1>Add weight</h1>
-					<input
-						type="text"
-						placeholder="Your weight in lbs"
-						{...register("weightTotal")}
-					/>
-					<br />
-					<textarea placeholder="Your post title" {...register("body")} />
-					<br />
-					<button>Create post</button>
+					<div className="mb-6">
+						<label className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">
+							Current weight in lbs
+						</label>
+						<input
+							type="text"
+							className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+							placeholder="180lbs"
+							required
+							{...register("weightTotal")}
+						/>
+					</div>
+					<div className="mb-6">
+						<label className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">
+							Description
+						</label>
+						<textarea
+							className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+							placeholder="Your post title"
+							{...register("body")}
+						/>
+					</div>
+					<button className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+						Add Weight
+					</button>
 				</form>
+
+				<table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+					<thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+						<tr>
+							<th scope="col" className="py-3 px-6">
+								Date
+							</th>
+							<th scope="col" className="py-3 px-6">
+								Weight
+							</th>
+							<th scope="col" className="py-3 px-6">
+								Description/Image
+							</th>
+						</tr>
+					</thead>
+
+					<tbody>
+						{data?.map((weight) => {
+							return (
+								<tr
+									key={weight.id}
+									className="bg-white border-b dark:bg-gray-800 dark:border-gray-700"
+								>
+									<th
+										scope="row"
+										className="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+									>
+										{weight.createdAt.toUTCString()}
+									</th>
+									<td className="py-4 px-6">{weight.weightTotal}</td>
+									<td className="py-4 px-6">{weight.body}</td>
+								</tr>
+							);
+						})}
+					</tbody>
+				</table>
 			</main>
 		</>
 	);
