81c391e901f6ca69f188079337a34a757418af37
zachuri <punsalang.zachary@gmail.com>
2022-09-03 11:35:23 -0700

    weightPage: added conditional statment to show form whether user is signed in or not

--- src/pages/weight.tsx
+++ src/pages/weight.tsx
@@ -7,8 +7,11 @@ import { useEffect, useState } from "react";
 import { useSetState } from "@mantine/hooks";
 import { Weight } from "@prisma/client";
 import { useQueryClient } from "react-query";
+import { useSession } from "next-auth/react";
 
 const Weight = () => {
+	const { data: session } = useSession();
+
 	const queryClient = useQueryClient();
 
 	const { handleSubmit, register, reset } = useForm<CreateWeightInput>();
@@ -37,69 +40,82 @@ const Weight = () => {
 	return (
 		<>
 			<main className="container mx-auto flex flex-col items-center justify-center min-h-screen p-4">
-				<form onSubmit={handleSubmit(onSubmit)}>
-					<div className="mb-6">
-						<label className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">
-							Current weight in lbs
-						</label>
-						<input
-							type="text"
-							className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-							placeholder="180lbs"
-							required
-							{...register("weightTotal")}
-						/>
-					</div>
-					<div className="mb-6">
-						<label className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">
-							Description
-						</label>
-						<textarea
-							className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-							placeholder="Your post title"
-							{...register("body")}
-						/>
-					</div>
-					<button className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-						Add Weight
-					</button>
-				</form>
+				{!session ? (
+					<>
+						<h1 className="text-5xl md:text-[5rem] leading-normal font-extrabold text-gray-700">
+							Weight Tracker
+						</h1>
+						<p className="text-2xl text-gray-700">Please Sign in!</p>
+					</>
+				) : (
+					<>
+						<form onSubmit={handleSubmit(onSubmit)}>
+							<div className="mb-6">
+								<label className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">
+									Current weight in lbs
+								</label>
+								<input
+									type="text"
+									className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+									placeholder="180lbs"
+									required
+									{...register("weightTotal")}
+								/>
+							</div>
+							<div className="mb-6">
+								<label className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">
+									Description
+								</label>
+								<textarea
+									className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+									placeholder="Your post title"
+									{...register("body")}
+								/>
+							</div>
+							<button className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+								Add Weight
+							</button>
+						</form>
 
-				<table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-					<thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-						<tr>
-							<th scope="col" className="py-3 px-6">
-								Date
-							</th>
-							<th scope="col" className="py-3 px-6">
-								Weight
-							</th>
-							<th scope="col" className="py-3 px-6">
-								Description/Image
-							</th>
-						</tr>
-					</thead>
-
-					<tbody>
-						{data?.map((weight) => {
-							return (
-								<tr
-									key={weight.id}
-									className="bg-white border-b dark:bg-gray-800 dark:border-gray-700"
-								>
-									<th
-										scope="row"
-										className="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-									>
-										{weight.createdAt.toUTCString()}
+						<table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+							<thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+								<tr>
+									<th scope="col" className="py-3 px-6">
+										Date
 									</th>
-									<td className="py-4 px-6">{weight.weightTotal}</td>
-									<td className="py-4 px-6">{weight.body}</td>
+									<th scope="col" className="py-3 px-6">
+										Weight
+									</th>
+									<th scope="col" className="py-3 px-6">
+										Description/Image
+									</th>
 								</tr>
-							);
-						})}
-					</tbody>
-				</table>
+							</thead>
+
+							<tbody>
+								{data?.map((weight) => {
+									return (
+										<tr
+											key={weight.id}
+											className="bg-white border-b dark:bg-gray-800 dark:border-gray-700"
+										>
+											<th
+												scope="row"
+												className="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+											>
+												{weight.createdAt.toUTCString()}
+											</th>
+											<td className="py-4 px-6">
+												{weight.weightTotal}
+											</td>
+											<td className="py-4 px-6">{weight.body}</td>
+										</tr>
+									);
+								})}
+							</tbody>
+						</table>
+					</>
+				)}
 			</main>
 		</>
 	);
