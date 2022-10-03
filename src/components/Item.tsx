994c5ac3e65f3d929d046dde5f84205fa72fce69
Zachary <punsalang.zachary@gmail.com>
2022-10-03 01:43:40 +0000

    item/weight: changed to grid system

--- src/components/Item.tsx
+++ src/components/Item.tsx
@@ -8,11 +8,13 @@ interface Props {
 
 const Item: React.FC<Props> = ({ date, weight, description }) => {
   return (
-    <div className='rounded-xl border border-black p-4 mt-3'>
-      <div>Date: {date}</div>
-      <div>Weight: {weight}</div>
-      <div>Description: {description}</div>
-    </div>
+    <>
+      <div className="rounded-xl border border-black p-4">
+        <div>Date: {date}</div>
+        <div>Weight: {weight}</div>
+        <div>Description: {description}</div>
+      </div>
+    </>
   )
 }
 
--- src/pages/weight.tsx
+++ src/pages/weight.tsx
@@ -73,7 +73,7 @@ const Weight = () => {
                   {...register("body")}
                 />
               </div>
-              <button className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+              <button className="mb-2 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                 Add Weight
               </button>
             </form>
@@ -115,13 +115,19 @@ const Weight = () => {
                 })}
               </tbody>
             </table> */}
-            {data?.map((weight) => {
-              return (
-                <div key={weight.id}>
-                  <Item weight={weight.weightTotal} date={weight.createdAt.toUTCString()} description={weight.body} />
-                </div>
-              )
-            })}
+            <div className="grid grid-cols-2 gap-3">
+              {data?.map((weight) => {
+                return (
+                  <div key={weight.id} >
+                    <Item
+                      weight={weight.weightTotal}
+                      date={weight.createdAt.toUTCString()}
+                      description={weight.body}
+                    />
+                  </div>
+                )
+              })}
+            </div>
           </>
         )}
       </main>
