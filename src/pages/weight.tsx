a47161c47c082eaab60cb244633f7c57b5cefb24
Zachary <punsalang.zachary@gmail.com>
2022-10-03 02:31:14 +0000

    weight: fixed layout made grid system for mobile and website

--- src/pages/weight.tsx
+++ src/pages/weight.tsx
@@ -40,7 +40,7 @@ const Weight = () => {
 
   return (
     <>
-      <main className="container mx-auto flex flex-col items-center justify-center min-h-screen p-4">
+      <main className="container mx-auto flex flex-col items-center justify-center pt-20">
         {!session ? (
           <>
             <h1 className="text-5xl md:text-[5rem] leading-normal font-extrabold text-gray-700">
@@ -115,7 +115,7 @@ const Weight = () => {
                 })}
               </tbody>
             </table> */}
-            <div className="grid grid-cols-2 gap-3">
+            <div className="grid grid-cols md:grid-cols-2 gap-3">
               {data?.map((weight) => {
                 return (
                   <div key={weight.id} >
